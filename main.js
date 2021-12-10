a52082c1e9fa22403dc21e99f20cee8b1b5ba82a
Rupert Foggo McKay <maxfmckay@gmail.com>
2021-12-10 18:16:23 +0100

    🦥 Maybe it works now?

--- main.js
+++ main.js
@@ -1,38 +1,28 @@
-// https://www.itacanet.org/the-sun-as-a-source-of-energy/part-3-calculating-solar-angles/
 const currentTime = new Date();
 const currentHour = currentTime.getHours();
 const monthOffsets = [0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334];
 const daysSinceNewYear =
   monthOffsets[currentTime.getMonth()] + currentTime.getDate();
 
-// declination angle
-const delta =
-  23.45 *
-  (Math.PI / 180) *
-  Math.sin(2 * Math.PI * ((285 + daysSinceNewYear) / 36.25));
+// The total tilt of the Earth
+const declinationMax = 0.4091; // Radians
+// Current tilt
+const declination =
+  -declinationMax * Math.cos(((2 * Math.PI) / 365) * (daysSinceNewYear + 10));
 
-// Observer's latitude
-const phi = 0.9076; // Delft's latitude in radians
+// Time before solar noon is negative, noon is zero, afternoon is positive
+const solarHour = 0.2618 * (currentHour - 12); // Radians
 
-// Solar hour angle in radians NB: sometimes denoted with 'h'
-const omega = 0.2618 * (currentHour - 12);
+const latitude = 0.9076; // Delft's latitude in radians
 
-// Altitude angle
-// const alpha = Math.asin(
-//   // TODO beware the inverse sin has two possible results, only one of which is correct
-//   Math.sin(delta) * Math.sin(phi) +
-//     Math.cos(delta) * Math.cos(omega) * Math.cos(phi)
-// );
+const cosSolarZenithAngle =
+  Math.sin(latitude) * Math.sin(declination) +
+  Math.cos(latitude) * Math.cos(declination) * Math.cos(solarHour);
 
-// Zenith angle
-const theta =
-  Math.sin(phi) * Math.sin(delta) +
-  Math.cos(phi) * Math.cos(delta) * Math.cos(omega);
+const cosAzimuth =
+  (Math.sin(declination) - cosSolarZenithAngle * Math.sin(latitude)) /
+  (Math.sin(Math.acos(cosSolarZenithAngle)) * Math.cos(latitude));
 
-// https://en.wikipedia.org/wiki/Solar_azimuth_angle#Conventional_Trigonometric_Formulas
-const azimuth = Math.acos(
-  (Math.sin(delta) - Math.cos(theta) * Math.sin(phi)) /
-    (Math.sin(theta) * Math.cos(phi))
-);
-
-console.log({ delta, phi, omega, theta, azimuth });
+// Measures starting from North running clockwise for 2PI
+const azimuth = Math.acos(cosAzimuth) + (solarHour < 0 ? 0 : Math.PI);
+console.log((180 / Math.PI) * azimuth);
