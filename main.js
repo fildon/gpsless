417fa469a2e120d84a59b0afa05bbc66c7c03b9b
Rupert Foggo McKay <maxfmckay@gmail.com>
2021-12-10 18:51:45 +0100

    🦣 Render information to DOM

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 const currentTime = new Date();
 const currentHour = currentTime.getHours();
+const currentMinute = currentTime.getMinutes();
+const currentSecond = currentTime.getSeconds();
 const monthOffsets = [0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334];
 const daysSinceNewYear =
   monthOffsets[currentTime.getMonth()] + currentTime.getDate();
@@ -11,7 +13,10 @@ const declination =
   -declinationMax * Math.cos(((2 * Math.PI) / 365) * (daysSinceNewYear + 10));
 
 // Time before solar noon is negative, noon is zero, afternoon is positive
-const solarHour = 0.2618 * (currentHour - 12); // Radians
+const solarHour =
+  ((2 * Math.PI) / 24) * (currentHour - 12) +
+  ((2 * Math.PI) / (24 * 60)) * currentMinute +
+  ((2 * Math.PI) / (24 * 60 * 60)) * currentSecond; // Radians
 
 const latitude = 0.9076; // Delft's latitude in radians
 
@@ -25,4 +30,6 @@ const cosAzimuth =
 
 // Measures starting from North running clockwise for 2PI
 const azimuth = Math.acos(cosAzimuth) + (solarHour < 0 ? 0 : Math.PI);
-console.log((180 / Math.PI) * azimuth);
+const azimuth_degrees = (180 / Math.PI) * azimuth;
+
+document.getElementById("azimuth").innerText = azimuth_degrees.toFixed(2);
