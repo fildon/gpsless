039fbc0e8a5ae4400bc2adb1197076790bb3b4ae
Rupert Foggo McKay <maxfmckay@gmail.com>
2022-01-22 18:34:10 +0100

    🥠 Now it works

--- main.js
+++ main.js
@@ -1,61 +1,74 @@
+const { acos, asin, cos, PI, sin } = Math;
+
+const degToRad = (degrees) => PI * (degrees / 180);
+
 /**
- * @param latitude (radians)
- * @returns azimuth in radians
+ * @see http://mypages.iit.edu/~maslanka/SolarGeo.pdf
  */
-const getAzimuth = (latitude) => {
-  const currentTime = new Date();
+const getAzimuth = (latitudeRads, currentTime = new Date()) => {
+  const phiRads = latitudeRads;
   const currentHour = currentTime.getHours();
   const currentMinute = currentTime.getMinutes();
   const currentSecond = currentTime.getSeconds();
+  const omegaRads =
+    ((2 * PI) / 24) * (currentHour - 12) +
+    ((2 * PI) / (24 * 60)) * currentMinute +
+    ((2 * PI) / (24 * 60 * 60)) * currentSecond;
+
   const monthOffsets = [0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334];
   const daysSinceNewYear =
     monthOffsets[currentTime.getMonth()] + currentTime.getDate();
 
-  // The total tilt of the Earth
-  const declinationMax = 0.4091; // Radians
-  // Current tilt
-  const declination =
-    -declinationMax * Math.cos(((2 * Math.PI) / 365) * (daysSinceNewYear + 10));
+  const deltaRads = asin(
+    0.39795 * cos(degToRad(0.98563 * (daysSinceNewYear - 173)))
+  );
 
-  // Time before solar noon is negative, noon is zero, afternoon is positive
-  const solarHour =
-    ((2 * Math.PI) / 24) * (currentHour - 12) +
-    ((2 * Math.PI) / (24 * 60)) * currentMinute +
-    ((2 * Math.PI) / (24 * 60 * 60)) * currentSecond; // Radians
+  const alphaRads = asin(
+    sin(deltaRads) * sin(phiRads) +
+      cos(deltaRads) * cos(omegaRads) * cos(phiRads)
+  );
 
-  const cosSolarZenithAngle =
-    Math.sin(latitude) * Math.sin(declination) +
-    Math.cos(latitude) * Math.cos(declination) * Math.cos(solarHour);
+  const azimuthRads = acos(
+    (sin(deltaRads) * cos(phiRads) -
+      cos(deltaRads) * cos(omegaRads) * sin(phiRads)) /
+      cos(alphaRads)
+  );
 
-  const cosAzimuth =
-    (Math.sin(declination) - cosSolarZenithAngle * Math.sin(latitude)) /
-    (Math.sin(Math.acos(cosSolarZenithAngle)) * Math.cos(latitude));
-
-  // Measures starting from North running clockwise for 2PI
-  const azimuth = Math.acos(cosAzimuth) + (solarHour < 0 ? 0 : Math.PI);
-  return azimuth;
+  // Astronomic convention: 0 = North, 0.5PI = East, PI = South, 1.5PI = West
+  return omegaRads <= 0 ? azimuthRads : 2 * PI - azimuthRads;
 };
 
-const selectElement = document.getElementById("location-select");
-const azimuthLine = document.getElementById("solarline");
-azimuthLine.setAttribute("stroke-width", "0.1");
-const solaricon = document.getElementById("solaricon");
-solaricon.setAttribute("dominant-baseline", "middle");
-solaricon.setAttribute("text-anchor", "middle");
-let interval;
-selectElement.addEventListener("change", (event) => {
-  clearInterval(interval);
-  const latitude = parseFloat(event.target.value);
-  if (Number.isNaN(latitude)) return;
+const getAzimuthLine = () => document.getElementById("solarline");
+const getSolarIcon = () => document.getElementById("solaricon");
 
-  interval = setInterval(() => {
-    const azimuth = getAzimuth(latitude);
-    // We flip from astronomic angles to mathematical conventional angles
-    const azimuth_x = Math.sin(azimuth);
-    const azimuth_y = -1 * Math.cos(azimuth);
-    azimuthLine.setAttribute("x2", azimuth_x);
-    azimuthLine.setAttribute("y2", azimuth_y);
-    solaricon.setAttribute("x", azimuth_x);
-    solaricon.setAttribute("y", azimuth_y);
-  }, 100);
-});
+const resetUi = () => {
+  getAzimuthLine().setAttribute("x2", "0");
+  getAzimuthLine().setAttribute("y2", "0");
+  getSolarIcon().setAttribute("x", "0");
+  getSolarIcon().setAttribute("y", "0");
+};
+
+// Astronomic convention: 0 = North, 0.5PI = East, PI = South, 1.5PI = West
+const updateUi = (azimuth) => {
+  const azimuth_x = sin(azimuth);
+  // We need to flip since screen y points down rather than up
+  const azimuth_y = -cos(azimuth);
+  getAzimuthLine().setAttribute("x2", azimuth_x);
+  getAzimuthLine().setAttribute("y2", azimuth_y);
+  getSolarIcon().setAttribute("x", azimuth_x);
+  getSolarIcon().setAttribute("y", azimuth_y);
+};
+
+let interval;
+document
+  .getElementById("location-select")
+  .addEventListener("change", (event) => {
+    clearInterval(interval);
+    const latitude = parseFloat(event.target.value);
+    if (Number.isNaN(latitude)) return resetUi();
+
+    interval = setInterval(() => {
+      const azimuth = getAzimuth(latitude);
+      updateUi(azimuth);
+    }, 100);
+  });
